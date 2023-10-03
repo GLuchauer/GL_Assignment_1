b444cb71a5ca63157524e709db309df06bbe5e36
GLuchauer <143447855+GLuchauer@users.noreply.github.com>
2023-10-03 12:35:10 -0500

    update

--- Simple Model_Squirrel.R
+++ Simple Model_Squirrel.R
@@ -9,14 +9,26 @@ maxsearch= 10
 lowsuccess= .9
 highsuccess= .1
 
+#food avaliability
+food = 1 #if present
+rate of successful food = .7 
+
 #runvars = Replicates()
 #remove()
 
 #run model iterating over parameters in Replicates
 #for(r in 1:nrow(runvars)){
    #initialize population of herbivores
-herbs = data.frame(id = seq(1,K,1), size = rep(seq(1,maxsize,1), (K/maxsize))
- herbs$search = 
+#herbs = data.frame(id = seq(1,K,1), size = rep(seq(1,maxsize,1), (K/maxsize))
+ #herbs$search = herbs$size+rnorm(K,0,1) 
+
+#search times 
+#herbs$search[herbs$search < 0] = 0
+#herbs$search[herbs$search > 10] = 10
+
+
+#
+#success = food(1-(lowsuccess-highsuccess)*searchresults[rr]))
    
    #search for food (relationship between time searching and finding food)
    #success or not in search
@@ -27,6 +39,6 @@ herbs = data.frame(id = seq(1,K,1), size = rep(seq(1,maxsize,1), (K/maxsize))
    ###TBD: how will we record the food quality the indivdual finds each year/unit of time
    ###TBD: how will we introduce food quality to this and how will it be set
                                      
-#}
+
 
 #end
